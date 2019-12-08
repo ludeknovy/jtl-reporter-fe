692f789277492675a6c5bc305488bd72575db66c
Luděk Nový <ludek@Ludek-MacBook-Pro.local>
2019-12-08 14:41:05 +0100

    loading animation

--- src/app/graphs/label-errors.ts
+++ src/app/graphs/label-errors.ts
@@ -1,42 +1,40 @@
 import { colors } from './colors';
 
-export const responseCodesChart = (data) => {
-  return {
-    chart: {
-      plotBackgroundColor: null,
-      plotBorderWidth: 0,
-      plotShadow: false
-    },
-    title: {
-      text: data.length > 0 ? 'Response codes' : 'no data',
-      align: 'center',
-      verticalAlign: 'middle',
-      y: 60
-    },
-    tooltip: {
-      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-    },
-    plotOptions: {
-      pie: {
-        colors: colors,
-        dataLabels: {
-          enabled: true,
-          style: {
-            fontWeight: 'bold',
-            color: 'black'
-          }
-        },
-        startAngle: -90,
-        endAngle: 90,
-        center: ['50%', '75%'],
-        size: '110%'
-      }
-    },
-    series: [{
-      type: 'pie',
-      name: 'Response codes',
-      innerSize: '50%',
-      data
-    }]
-  };
+export const emptyResponseCodesChart = {
+  chart: {
+    plotBackgroundColor: null,
+    plotBorderWidth: 0,
+    plotShadow: false
+  },
+  colors,
+  title: {
+    text: 'Response codes',
+    align: 'center',
+    verticalAlign: 'middle',
+    y: 60
+  },
+  tooltip: {
+    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+  },
+  plotOptions: {
+    pie: {
+      dataLabels: {
+        enabled: true,
+        style: {
+          fontWeight: 'bold',
+          color: 'black'
+        }
+      },
+      startAngle: -90,
+      endAngle: 90,
+      center: ['50%', '75%'],
+      size: '110%'
+    }
+  },
+  series: [{
+    type: 'pie',
+    name: 'Response codes',
+    innerSize: '50%',
+  }],
 };
+
--- src/app/item-detail/label-error/label-error.component.ts
+++ src/app/item-detail/label-error/label-error.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { LabelApiService } from 'src/app/label-api.service';
 import * as Highcharts from 'highcharts';
-import { responseCodesChart } from 'src/app/graphs/label-errors';
+import { emptyResponseCodesChart } from 'src/app/graphs/label-errors';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-label-error',
@@ -13,7 +14,7 @@ export class LabelErrorComponent implements OnInit {
 
   @Input() labelInput: any;
   Highcharts: typeof Highcharts = Highcharts;
-  chart;
+  chart: Highcharts.Chart;
   chartConstructor = 'chart';
   chartCallback;
   updateFlag = false;
@@ -25,6 +26,7 @@ export class LabelErrorComponent implements OnInit {
     private labelApiService: LabelApiService,
   ) {
     const self = this;
+
     this.chartCallback = chart => {
       self.chart = chart;
     };
@@ -33,18 +35,33 @@ export class LabelErrorComponent implements OnInit {
   ngOnInit() {
   }
 
+
   open(content) {
     // @ts-ignore
     this.modalService.open(content, { size: 'xl' }).result
-      .then((_) => { this.labelErrorChartOption = null; }, () => { this.labelErrorChartOption = null; });
+    .then((_) => { this.labelErrorChartOption = null; }, () => { this.labelErrorChartOption = null; });
+
     const { projectName, scenarioName, id } = this.labelInput.params;
-    this.labelApiService.fetchLabelErrors(projectName, scenarioName, id, this.labelInput.labelName)
-      .subscribe((_) => {
-        const errors = Object.keys(_.stat).map((e) => {
-          return [e, _.stat[e]];
+
+
+    from(new Promise(resolve => setTimeout(resolve, 100))).subscribe((val: any) => {
+      this.labelErrorChartOption = emptyResponseCodesChart;
+    });
+    from(new Promise(resolve => setTimeout(resolve, 300))).subscribe((val: any) => {
+
+      this.chart.showLoading();
+
+      this.labelApiService.fetchLabelErrors(projectName, scenarioName, id, this.labelInput.labelName)
+        .subscribe((_) => {
+          this.chart.hideLoading();
+          const errors = Object.keys(_.stat).map((e) => {
+            return [e, _.stat[e]];
+          });
+          this.chart.series[0].setData(errors);
         });
-        this.labelErrorChartOption = responseCodesChart(errors);
-      });
+    });
   }
+
+
 
 }

c263a5d990a1ae08a6090d60c5974f564eb17b6a
Ludek Novy <ludeknovy@fastmail.com>
2021-04-08 17:57:31 +0200

    test fix

--- src/app/scenario/scenario-trends/scenario-trends.component.spec.ts
+++ src/app/scenario/scenario-trends/scenario-trends.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HighchartsChartModule } from 'highcharts-angular';
 
@@ -16,6 +17,7 @@ describe('ScenarioTrendsComponent', () => {
         HighchartsChartModule,
         NgbModule,
         HttpClientTestingModule,
+        RouterTestingModule,
       ]
     })
       .compileComponents();

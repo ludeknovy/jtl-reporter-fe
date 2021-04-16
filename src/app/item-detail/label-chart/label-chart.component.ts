617cca1ea13e5fbca00e338b843bd823ef302805
Ludek Novy <ludeknovy@fastmail.com>
2021-04-16 11:08:47 +0200

    label chart collapsable

--- src/app/item-detail/label-chart/label-chart.component.ts
+++ src/app/item-detail/label-chart/label-chart.component.ts
@@ -11,7 +11,6 @@ export class LabelChartComponent implements OnInit {
 
   @Input() labels: Map<string, object>;
   Highcharts: typeof Highcharts = Highcharts;
-  HighchartsCompare: typeof Highcharts = Highcharts;
   chartConstructor = 'chart';
   labelChartMetric = 'Throughput';
   labelCompareChartMetric;
@@ -20,6 +19,8 @@ export class LabelChartComponent implements OnInit {
   chartKeys;
   seriesVisibilityToggle = true;
   seriesVisibilityToggleText = 'Hide all';
+  chartShouldExpand = false;
+  chart;
 
   constructor() { }
 
@@ -40,12 +41,20 @@ export class LabelChartComponent implements OnInit {
     this.updateLabelChartFlag = true;
   }
 
-  toggleSeriesVisibility(event) {
+  toggleSeriesVisibility() {
     this.labelChartOptions.series.map(_ => _.visible = !this.seriesVisibilityToggle);
     this.seriesVisibilityToggleText = this.seriesVisibilityToggle ? 'Show all' : 'Hide all';
     this.seriesVisibilityToggle = !this.seriesVisibilityToggle;
     this.updateLabelChartFlag = true;
-
   }
 
+  collapseChart() {
+    this.chartShouldExpand = !this.chartShouldExpand;
+    this.chart.setSize(undefined, this.chartShouldExpand ? 650 : 350);
+  }
+
+
+  getInstance(chart): void {
+    this.chart = chart;
+ }
 }

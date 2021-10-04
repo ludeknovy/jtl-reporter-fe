9b3a2d334fa5153ff91f9a86fd0406489b24c8d5
Ludek Novy <ludeknovy@fastmail.com>
2021-10-04 21:20:56 +0200

    status codes distribution

---
+++ src/app/graphs/status-codes.ts
@@ -0,0 +1,32 @@
+export const statusCodesChart = (points) => {
+  return {
+    exporting: {
+      enabled: false,
+    },
+    series: [{
+      type: 'treemap',
+      // layoutAlgorithm: 'squarified',
+      allowDrillToNode: false,
+      animationLimit: 1000,
+      dataLabels: {
+        enabled: false
+      },
+      levelIsConstant: true,
+      levels: [{
+        level: 1,
+        dataLabels: {
+          enabled: true
+        },
+        borderWidth: 3
+      }],
+      data: points
+    }],
+    subtitle: {
+      text: ''
+    },
+    title: {
+      text: ''
+    }
+  };
+};
+

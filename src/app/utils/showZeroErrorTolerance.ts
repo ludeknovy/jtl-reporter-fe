241b38d1a7cd31a051ee59052efa32c301b4b753
Luděk Nový <13610612+ludeknovy@users.noreply.github.com>
2021-12-09 13:26:38 +0100

    zero error tolerance fix

--- src/app/utils/showZeroErrorTolerance.ts
+++ src/app/utils/showZeroErrorTolerance.ts
@@ -2,7 +2,7 @@ export const showZeroErrorWarning = (errorRate, errorCount) => {
   if (errorRate > 0) {
     return true;
   } else {
-    if (!errorCount) {
+    if (errorCount === null || errorCount === undefined) {
       return 'unknown';
     }
     return errorCount && errorCount > 0;

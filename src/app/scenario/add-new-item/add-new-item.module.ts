9a603636daffd61476ee14bfce54bd481bd9313b
Luděk Nový <13610612+ludeknovy@users.noreply.github.com>
2022-01-27 14:41:52 +0100

    missing import fix

--- src/app/scenario/add-new-item/add-new-item.module.ts
+++ src/app/scenario/add-new-item/add-new-item.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { RoleModule } from "src/app/_directives/role.module";
 import { AddNewItemComponent } from "./add-new-item.component";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [AddNewItemComponent],
   imports: [
-    CommonModule, RouterModule, RoleModule,
+    CommonModule, RouterModule, RoleModule, ReactiveFormsModule,
   ],
   exports: [AddNewItemComponent]
 })

059cc2ee7b4de0974c4721f335297ff9d0716b32
Luděk Nový <13610612+ludeknovy@users.noreply.github.com>
2022-01-28 22:18:43 +0100

    test update

--- src/app/administration/navigation/navigation.component.spec.ts
+++ src/app/administration/navigation/navigation.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
+import { RoleDirective } from "src/app/_directives/role.directive";
 
 
 import { NavigationComponent } from "./navigation.component";
@@ -11,9 +12,19 @@ describe("NavigationComponent", () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [ NavigationComponent ]
+      declarations: [ NavigationComponent, RoleDirective ]
     })
     .compileComponents();
+
+  const mockLocalStorage = {
+    getItem: (key: string): string => {
+      return JSON.stringify({ role: "operator"})
+    },
+  };
+
+  spyOn(localStorage, 'getItem')
+    .and.callFake(mockLocalStorage.getItem);
+
   }));
 
   beforeEach(() => {

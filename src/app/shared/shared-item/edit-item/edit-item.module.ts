e611f7d5616e43ef78ccd9d262bbf5b116fc88e1
Luděk Nový <13610612+ludeknovy@users.noreply.github.com>
2022-02-01 19:03:31 +0100

    base run tooltip

--- src/app/shared/shared-item/edit-item/edit-item.module.ts
+++ src/app/shared/shared-item/edit-item/edit-item.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from "@angular/common";
 import { RoleModule } from "src/app/_directives/role.module";
 import { EditItemComponent } from "./edit-item.component";
 import { ReactiveFormsModule } from "@angular/forms";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [EditItemComponent],
   imports: [
-    CommonModule, RoleModule, ReactiveFormsModule,
+    CommonModule, RoleModule, ReactiveFormsModule, NgbModule
   ],
   exports: [EditItemComponent]
 })

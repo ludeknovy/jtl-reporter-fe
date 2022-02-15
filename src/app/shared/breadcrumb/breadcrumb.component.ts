81bea7a974da0163b7771e71bd1b0b189061d17e
Luděk Nový <13610612+ludeknovy@users.noreply.github.com>
2022-02-15 10:18:00 +0100

    test name

--- src/app/shared/breadcrumb/breadcrumb.component.ts
+++ src/app/shared/breadcrumb/breadcrumb.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewEncapsulation } from "@angular/core";
-import { ActivatedRoute, NavigationEnd, Router } from "@angular/router";
+import { Component, Input, OnChanges, OnInit, SimpleChanges, ViewEncapsulation } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-breadcrumb",
@@ -7,18 +7,28 @@ import { ActivatedRoute, NavigationEnd, Router } from "@angular/router";
   styleUrls: ["./breadcrumb.component.css"],
   encapsulation: ViewEncapsulation.None
 })
-export class BreadcrumbComponent implements OnInit {
+export class BreadcrumbComponent implements OnInit, OnChanges {
+  @Input() testName
+
   params;
   urls = [];
 
   constructor(private route: ActivatedRoute) {
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+      const { testName } = changes
+      if (testName.currentValue !== testName.previousValue) {
+        this.urls[this.urls.length - 1] = { label: this.testName || "test run", url: "", last: true }
+      }
+  }
+
   ngOnInit() {
-    this.route.params.subscribe(_ => {
+
+    this.route.params.subscribe(param => {
       this.urls = [];
-      const { projectName = null, scenarioName = null, id = null } = _;
-      const _length = Object.keys(_).length;
+      const { projectName = null, scenarioName = null, id = null } = param;
+      const _length = Object.keys(param).length;
       if (projectName) {
         const projectUrlTemplate = `/project/${projectName}/scenarios`;
         this.urls.push({ label: projectName, url: projectUrlTemplate, last: _length === 1 });
@@ -28,7 +38,7 @@ export class BreadcrumbComponent implements OnInit {
         this.urls.push({ label: scenarioName, url: projectUrlTemplate, last: _length === 2 });
       }
       if (id) {
-        this.urls.push({ label: "test run", url: "", last: _length === 3 });
+        this.urls.push({ label: this.testName || "test run", url: "", last: _length === 3 });
       }
     });
   }

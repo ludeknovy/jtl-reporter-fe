1bafac0df50d44673674b9479bfd7a63752ba993
Luděk Nový <13610612+ludeknovy@users.noreply.github.com>
2022-02-28 16:25:39 +0100

    tooltip unit suffix, custom chart max 2 decimals fix

--- src/app/graphs/item-detail.ts
+++ src/app/graphs/item-detail.ts
@@ -126,7 +126,8 @@ export const customChartSettings = () => {
     "#707B7C", "#7DCEA0", "#21618C", "#873600", "#AF7AC5", "#B7950B"],
   tooltip: {
     split: true,
-    crosshairs: [true]
+    crosshairs: [true],
+    valueDecimals: 2,
   },
   plotOptions: {
     series: {

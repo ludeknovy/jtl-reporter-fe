57ca19cb44773bbb2dcee58a01a194971b14f55b
Ludek Novy <13610612+ludeknovy@users.noreply.github.com>
2022-10-14 13:55:19 +0200

    removing pact + contract tests, updating dependencies (#263)

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -3,41 +3,32 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular', 'pact'],
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-      require('@pact-foundation/karma-pact'),
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage-istanbul-reporter"),
+      require("@angular-devkit/build-angular/plugins/karma"),
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly'],
+      dir: require("path").join(__dirname, "../coverage"),
+      reports: ["html", "lcovonly"],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ["progress", "kjhtml"],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ["Chrome"],
     singleRun: false,
-    pact: [{
-      cors: true,
-      port: 1234,
-      consumer: "jtl-reporter-ui",
-      provider: "jtl-reporter-be",
-      dir: "pacts",
-      spec: 2
-    }],
     proxies: {
-      '/projects': 'http://127.0.0.1:1234/projects'
+      "/projects": "http://127.0.0.1:1234/projects"
     }
   });
 };

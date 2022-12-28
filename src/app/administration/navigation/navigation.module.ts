f7efb0c9b3eb1e5a51ae3e74e613a6f07adcc8b3
Luděk Nový <13610612+ludeknovy@users.noreply.github.com>
2022-12-28 20:51:17 +0100

    administration revamped

--- src/app/administration/navigation/navigation.module.ts
+++ src/app/administration/navigation/navigation.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { NavigationComponent } from "./navigation.component";
-import { RouterModule } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 import { RoleModule } from "src/app/_directives/role.module";
+import { NgbNavModule } from "@ng-bootstrap/ng-bootstrap";
+import { AuthGuard } from "../../auth.guard";
+import { ProjectAdministrationModule } from "../projects/project-administration.module";
+import { ApiKeysModule } from "../api-token/api-keys.module";
+import { UsersModule } from "../users/users.module";
+import { MyProfileModule } from "../my-profile/my-profile.module";
+
+const routes: Routes = [
+  { path: "administration", component: NavigationComponent, canActivate: [AuthGuard] },
+];
 
 @NgModule({
   declarations: [NavigationComponent],
   imports: [
-    CommonModule, RouterModule, RoleModule,
+    CommonModule, RouterModule, RoleModule, NgbNavModule, RouterModule.forRoot(routes), ProjectAdministrationModule, ApiKeysModule,
+    UsersModule, MyProfileModule,
   ],
   exports: [NavigationComponent]
 })
-export class NavigationModule { }
+export class NavigationModule {
+}

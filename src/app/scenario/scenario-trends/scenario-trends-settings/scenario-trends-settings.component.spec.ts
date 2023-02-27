0db54e2d4628c9ab945fca5058dcb51526ad2ef6
Luděk Nový <13610612+ludeknovy@users.noreply.github.com>
2023-02-27 20:28:34 +0100

    text fix

--- src/app/scenario/scenario-trends/scenario-trends-settings/scenario-trends-settings.component.spec.ts
+++ src/app/scenario/scenario-trends/scenario-trends-settings/scenario-trends-settings.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { ScenarioTrendsSettingsComponent } from "./scenario-trends-settings.component";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
 describe("ScenarioTrendsSettingsComponent", () => {
   let component: ScenarioTrendsSettingsComponent;
@@ -8,14 +9,24 @@ describe("ScenarioTrendsSettingsComponent", () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ScenarioTrendsSettingsComponent ]
+      declarations: [ScenarioTrendsSettingsComponent],
+      imports: [HttpClientTestingModule]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ScenarioTrendsSettingsComponent);
     component = fixture.componentInstance;
+    component.userSettings = {
+      aggregatedTrends: true,
+      labelMetrics: {
+        percentile90: true,
+        errorRate: true,
+        throughput: false,
+      }
+    }
+    component.params = { scenarioName: "test", projectName: "test" };
     fixture.detectChanges();
   });
 

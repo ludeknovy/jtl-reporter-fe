36c6cabb0654fa531f0c91d598d5038984098a95
Luděk Nový <13610612+ludeknovy@users.noreply.github.com>
2024-03-07 10:21:53 +0100

    more bootstrap fixes

--- src/app/administration/my-profile/my-profile.module.ts
+++ src/app/administration/my-profile/my-profile.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from "@angular/common";
 import { SharedModule } from "../../shared/shared.module";
 import { ReactiveFormsModule } from "@angular/forms";
 import { MyProfileComponent } from "./my-profile.component";
+import {ProjectAdministrationModule} from '../projects/project-administration.module';
 
 
 
 @NgModule({
   declarations: [MyProfileComponent],
-  imports: [
-    CommonModule, SharedModule, ReactiveFormsModule
-  ],
+    imports: [
+        CommonModule, SharedModule, ReactiveFormsModule, ProjectAdministrationModule
+    ],
   exports: [MyProfileComponent]
 })
 export class MyProfileModule { }

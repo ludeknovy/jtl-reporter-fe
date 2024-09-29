79e8c557c668e6ac34280cef9244c892dae758e4
Ludek Novy <13610612+ludeknovy@users.noreply.github.com>
2024-09-29 12:01:22 +0200

    Sticky control panel (#421)

--- src/app/shared/control-panel/control-panel.component.ts
+++ src/app/shared/control-panel/control-panel.component.ts
@@ -1,16 +1,31 @@
-import { Component } from "@angular/core";
+import { Component, HostListener, Input } from "@angular/core";
 
 @Component({
-  selector: "app-control-panel",
-  templateUrl: "./control-panel.component.html",
-  styleUrls: ["./control-panel.component.css", "../../shared-styles.css"]
+  selector: 'app-control-panel',
+  templateUrl: './control-panel.component.html',
+  styleUrls: ['./control-panel.component.css', '../../shared-styles.css']
 })
 export class ControlPanelComponent {
+
+  @Input() shouldStick = false;
 
   navbarOpen = false;
 
   toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    if (!this.shouldStick) {
+      return
+    }
+    const element = document.querySelector("#top-panel") as HTMLElement;
+    if (window.pageYOffset > element.clientHeight) {
+      element.classList.add("navbar-inverse");
+    } else {
+      element.classList.remove("navbar-inverse");
+    }
   }
 
 }
